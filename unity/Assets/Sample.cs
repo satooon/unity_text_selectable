d6092068e3ab49487745e99e34a682cc1295beac
satooon <lucklaw9210@gmail.com>
2019-01-18 11:18:01 +0900

    Add UnityProject

---
+++ unity/Assets/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        this.ShowTextView();
+#endif
+    }
+
+    void ShowTextView()
+    {
+        using (var cls = new AndroidJavaClass("com.unity.plugin.ui.TextSelectable")) {
+            using (var textSelectable = cls.CallStatic<AndroidJavaObject>("Make", 500, 500, 0, 0)) {
+                textSelectable.Call("SetText", "test test test test test test test");
+            }
+        }
+    }
+}
