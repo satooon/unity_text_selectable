1fa060e8b8b9b1516089087b3246edaf98d65cd0
satooon <lucklaw9210@gmail.com>
2019-01-28 17:21:03 +0900

    Unity側iOSプラグイン呼び出し

---
+++ unity/Assets/Plugins/iOS/TextSelectable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TextSelectable
+{
+    [DllImport("__Internal")]
+    private static extern void make_();
+    [DllImport("__Internal")]
+    private static extern void setText_(string text);
+
+    public static void Make()
+    {
+        make_();
+    }
+    public static void SetText(string text)
+    {
+        setText_(text);
+    }
+}
