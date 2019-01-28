8170706a711ac059e42d0d8919e613c87ee39bde
satooon <lucklaw9210@gmail.com>
2019-01-28 17:21:10 +0900

    サンプルコード

--- unity/Assets/Sample.cs
+++ unity/Assets/Sample.cs
@@ -6,17 +6,27 @@ public class Sample : MonoBehaviour
 {
     void Start()
     {
-#if UNITY_ANDROID && !UNITY_EDITOR
-        this.ShowTextView();
+#if UNITY_EDITOR
+        Debug.Log("None");
+#elif UNITY_ANDROID
+        this.ShowTextViewByAndroid();
+#elif UNITY_IOS
+        this.ShowTextViewByiOS();
 #endif
     }
 
-    void ShowTextView()
+    void ShowTextViewByAndroid()
     {
         using (var cls = new AndroidJavaClass("com.unity.plugin.ui.TextSelectable")) {
             using (var textSelectable = cls.CallStatic<AndroidJavaObject>("Make", 500, 500, 0, 0)) {
                 textSelectable.Call("SetText", "test test test test test test test");
             }
         }
+    }
+
+    void ShowTextViewByiOS()
+    {
+        TextSelectable.Make();
+        TextSelectable.SetText("sample samplesamplesample sample sample sample sample");
     }
 }
